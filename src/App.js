166e0e7f76c83f8773f122565d333375af6aa2ed
Chiranjeevi Dasatti <chiranjeevi.dasatti@planonsoftware.com>
2023-05-30 02:44:34 +0530

    updated yml and app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Welcome to My App!</h1>
+      <h1>Welcome to My App! Chiru</h1>
       <p>This is a demo application for Azure DevOps pipelines.</p>
     </div>
   );
