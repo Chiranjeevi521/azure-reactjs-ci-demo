def9a5f4a77e3fb0c16bd4e336ca1e4c94f24c39
Chiranjeevi Dasatti <chiranjeevi.dasatti@planonsoftware.com>
2023-05-30 17:08:37 +0530

    Added test data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
 function App() {
+  const items = ['Apple', 'Banana', 'Cherry'];
   return (
     <div>
-      <h1>Welcome to My App! Chiru</h1>
+      <h1>Welcome to App!</h1>
       <p>This is a demo application for Azure DevOps pipelines.</p>
+      <div>
+        <h2>Let's have some fun!</h2>
+        <p>Here's a random joke for you:</p>
+        <p>
+          Q: Why don't scientists trust atoms?<br />
+          A: Because they make up everything!
+        </p>
+        <p>Hope that brought a smile to your face!</p>
+        <p>
+          If you have any questions or need further assistance, feel free to ask.
+          Happy coding!
+        </p>
+      </div>
+      <div>
+        <h2>Display ['Apple', 'Banana', 'Cherry'] in list format</h2>
+        <p>Uh-oh! There's a bug in the function below:</p>
+        <ul>
+          {items.map((item) => (
+            <li>{item.id}</li>
+          ))}
+        </ul>
+        <p>Can you spot the issue?</p>
+      </div>
     </div>
   );
 }
